43b6971bf45e785e73459418cd260f15c83b6d0e
t3codes <tharlles.engineer@gmail.com>
2025-07-02 22:46:46 -0300

    feat: Criando sistema de estado da aplicação usando o BehaviorSubject

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  standalone: true, // ✅ importante pra standalone
+  imports: [RouterModule, CommonModule], // ✅ Aqui adiciona o CommonModule
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
 export class Layout {
-
+  title = "testando 1 2 3"
 }
